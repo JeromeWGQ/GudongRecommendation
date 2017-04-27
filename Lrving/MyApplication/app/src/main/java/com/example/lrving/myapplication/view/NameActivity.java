36d83a3533616f30300040203e604015024a4ce4
l804775454 <14301128@bjtu.edu.cn>
2017-04-27 08:42:28 +0800

    4.27

---
+++ Lrving/MyApplication/app/src/main/java/com/example/lrving/myapplication/view/NameActivity.java
@@ -0,0 +1,44 @@
+package com.example.lrving.myapplication.view;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.lrving.myapplication.R;
+import com.example.lrving.myapplication.model.User;
+
+public class NameActivity extends AppCompatActivity {
+
+    private Button btn_nameCancel,btn_nameOk;
+    private EditText et_name;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_name);
+        getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);  //全屏
+
+        et_name = (EditText) findViewById(R.id.et_name);
+        et_name.setText(User.name);
+        btn_nameCancel = (Button) findViewById(R.id.btn_nameCancel);
+        btn_nameOk= (Button) findViewById(R.id.btn_nameOk);
+        btn_nameCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        btn_nameOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                User.name = String.valueOf(et_name.getText());
+                finish();
+            }
+        });
+    }
+}
