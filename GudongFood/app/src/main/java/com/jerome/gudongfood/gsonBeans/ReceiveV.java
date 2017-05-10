4df52446267ce74259296da40f25c22ccd3ef32b
王国桥 <wfdwgq2013@126.com>
2017-05-10 14:59:51 +0800

    commit0510 - 3

---
+++ GudongFood/app/src/main/java/com/jerome/gudongfood/gsonBeans/ReceiveV.java
@@ -0,0 +1,12 @@
+package com.jerome.gudongfood.gsonBeans;
+
+/**
+ * Created by Jerome on 2017/5/10.
+ */
+
+public class ReceiveV {
+    public String status;
+    public String vegetable1;
+    public String vegetable2;
+    public String vegetable3;
+}
